57200949f81e84d38a1566cef8317796fb1bffc3
chornenkj <xipypg2006@yahoo.com>
2022-11-20 18:43:48 +0000

    Django course 2, Week2 Assignment "templates" passed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. 8de91cce is the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question 6076036f %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question 6076036f %s." % question_id)
+
+def owner(request):
+    return HttpResponse("Hello, world. 6076036f is the polls index.")
+
