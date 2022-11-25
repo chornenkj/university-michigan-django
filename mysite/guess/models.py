093eb65cdba667d9101b58e91f8345bdf93145fe
chornenkj <xipypg2006@yahoo.com>
2022-11-25 15:35:52 +0000

    Comments added

--- mysite/guess/models.py
+++ mysite/guess/models.py
@@ -1,37 +1,53 @@
 from django.db import models
 
 
+# class to save a single game
 class Game(models.Model):
+    # True if the game is active, not ended
     ongoing = models.BooleanField(default=True)
+    # date game started
     date = models.DateTimeField(auto_now=True)
+    # correct number to guess
     guess_try = models.IntegerField()
 
+    # string representation for a game
     def __str__(self):
         return 'Game started {} with {} tries'#.format(str(self.date),str(self.guess_set.count()))
 
+    # function to check guess number
+    # returns a message to present on the page
     def check_guess(self, guess):
         msg = False
         if guess:
             try:
+                # create a guess object for current guess
                 self.guess_set.create(guess=guess)
+                # in case the guess is correct
                 if int(guess) < self.guess_try:
                     msg = 'Guess {} is too low'.format(str(guess))
                 elif int(guess) > self.guess_try:
                     msg = 'Guess {} is too high'.format(str(guess))
                 else:
                     msg = 'Congrats! {} is correct guess!\nYou used {} attempts!'#.format(str(guess),str(self.guess_set.count()))
+                    # set ongoing attribute to False (game ended) and save to database
                     self.ongoing = False
                     self.save()
+            # in case the guess is not correct
             except:
                 msg = 'Bad guess format!'
         return msg
 
 
+# class to save a single guess
 class Guess(models.Model):
+    # game object related to this guess
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    # guess number
     guess = models.IntegerField()
+    # date a guess was submitted
     date = models.DateTimeField(auto_now=True)
 
+    # string representation for a single guess
     def __str__(self):
         return '{} - guess {} ({}) for game {}'#.format(str(self.guess),str(self.id),str(self.date),str(self.game.id))
 
--- mysite/guess/views.py
+++ mysite/guess/views.py
@@ -7,46 +7,79 @@ from .models import Game
 import random
 
 
+# the view for main page, name='guess:index'
 def index(request):
     return render(request, 'guess/index.html')
 
 
+# the view for game page, name='guess:next_try'
 class TryView(View):
 
+    # view function for GET method
     def get(self, request):
+
+        # in case there is message saved in session (number was submitted and redirected from POST)
+        # save msg into variable and delete it from session
         msg = request.session.get('msg', False)
         if msg:
             del request.session['msg']
 
+        # in case there is no game_id saved in session (no ongoing game)
+        # perfom actions to start a new game:
         game_id = request.session.get('game_id', False)
         if not game_id:
+            # create new Game object and set a value to guess
             game = Game(guess_try = random.randint(0,500))
+            # save Game object to database
             game.save()
+            # save game_id to session
             request.session['game_id'] = game.id
 
+        # in case the number is guessed (ongoing attribute is set to False)
         start_new = False
         game = Game.objects.get(id=request.session.get('game_id', False))
         if not game.ongoing:
+            # delete previous game_id from session
             del request.session['game_id']
+            # create a variable to inform template to set a page for congrats and
+            # locate a link to start new game instead of form to submit a number
             start_new = True
 
+        # session data just for debugging
         current_session = {}
         for key, value in request.session.items():
             current_session[escape(key)] = escape(value)
 
+        # COOKIES values just for debugging
         current_cookies = {}
         for key, value in request.COOKIES.items():
             current_cookies[escape(key)] = escape(value)
 
+        # render a response using a template
+        # data:
+        # sn - True if previous game ended and offer to start new one
+        # message - message from redirected POST submit
+        # cs - current session data
+        # cc - current COOKIES data
         return render(request, 'guess/next_try.html', {'sn' : start_new, 'message' : msg, 'cs' : current_session, 'cc' : current_cookies})
 
-
+    # view function for POST method
+    # POST method is used if user submits guess number
     def post(self, request):
+
+        # first, get guess number from POST data
         guess = request.POST.get('guess')
+
+        # get game object using game_id from session
         game = Game.objects.get(id=request.session.get('game_id', False))
+
+        # if game is found check the guess, save the result into session
+        # and redirect browser to game page to use GET method
         if game:
             msg = game.check_guess(guess)
             request.session['msg'] = msg
             return redirect(request.path)
+
+        # if game is not found redirect to the main (home) page
         else:
             return redirect(reverse('guess:index'))
