81bd514c68dddbbb832e8ef3d0677d199372922b
Mykhaylo Chornenkyy <xipypg2006@yahoo.com>
2022-12-02 13:16:53 +0100

    Cosmrtic corrects

---
+++ python_codes/main.py
@@ -0,0 +1,13 @@
+# Script for PyCharm configuration to run other scripts
+
+from browse_dataorg import browse_dataorg
+from browse_localhost import browse_localhost
+from my_server import create_server
+
+
+
+# browse_dataorg()
+# browse_localhost()
+
+print('Access http://localhost:9000')
+create_server()
