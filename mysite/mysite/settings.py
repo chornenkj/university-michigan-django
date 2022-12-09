480af5064ed15e87759070ba6a893e2346f8dc79
Mykhaylo Chornenkyy <xipypg2006@yahoo.com>
2022-12-09 23:17:12 +0100

    Setting changes for the local notebook server

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'mysql': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'chornenkj$ads',
         'USER': 'chornenkj',
@@ -105,7 +105,7 @@ DATABASES = {
         },
     },
 
-    'sqlite3': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
