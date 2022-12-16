4b6dc82b7035130524dd8529cec5645b32b286a9
chornenkj <xipypg2006@yahoo.com>
2022-12-16 09:01:07 +0000

    Ads app. Add CommentForm to forms.py

--- mysite/ads/forms.py
+++ mysite/ads/forms.py
@@ -50,7 +50,20 @@ class CreateForm(forms.ModelForm):
 
         return instance
 
-# https://docs.djangoproject.com/en/3.0/topics/http/file-uploads/
-# https://stackoverflow.com/questions/2472422/django-file-upload-size-limit
-# https://stackoverflow.com/questions/32007311/how-to-change-data-in-django-modelform
-# https://docs.djangoproject.com/en/3.0/ref/forms/validation/#cleaning-and-validating-fields-that-depend-on-each-other
+"""
+References for CreateForm:
+https://docs.djangoproject.com/en/3.0/topics/http/file-uploads/
+https://stackoverflow.com/questions/2472422/django-file-upload-size-limit
+https://stackoverflow.com/questions/32007311/how-to-change-data-in-django-modelform
+https://docs.djangoproject.com/en/3.0/ref/forms/validation/#cleaning-and-validating-fields-that-depend-on-each-other
+"""
+
+
+# strip means to remove whitespace from the beginning and the end before storing the column
+class CommentForm(forms.Form):
+    comment = forms.CharField(
+        required=True,
+        max_length=500,
+        min_length=3,
+        strip=True
+    )
